7b356c1b41be3ea5f3eba440cac133c7e05c628d
Vinil-D <Vinil.dumpala@gmail.com>
2023-06-29 11:31:26 +0100

    modified api

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,11 @@
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
+    // Student details
+    const student = {
+        name: "John Doe",
+        age: 20,
+        grade: "A"
+    };
+
+    // Respond with student details in JSON format
+    context.res.json(student);
 };
